0ba45861f52e5df624132846e602d03f886935ed
Andrzej Grabowski <andys.grabek@icloud.com>
2019-11-03 22:54:18 +0100

    Show YT video preview in tutorial configuration

--- src/app/video-tutorial-editor/video-tutorial-editor.component.ts
+++ src/app/video-tutorial-editor/video-tutorial-editor.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AbstractTutorialEditor} from '../abstract-tutorial-editor/abstract-tutorial-editor';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-video-tutorial-editor',
@@ -8,11 +9,24 @@ import {AbstractTutorialEditor} from '../abstract-tutorial-editor/abstract-tutor
 })
 export class VideoTutorialEditorComponent extends AbstractTutorialEditor implements OnInit {
 
-  constructor() {
+  ytUrl: SafeResourceUrl;
+
+  constructor(private sanitizer: DomSanitizer) {
     super();
   }
 
   ngOnInit() {
+    this.sanitizeYouTubeUrl();
+  }
+
+  onUrlChange(value: string) {
+    super.onUrlChange(value);
+    this.sanitizeYouTubeUrl();
+  }
+
+  private sanitizeYouTubeUrl() {
+    // TODO allow non-embeddable urls from user input
+    this.ytUrl = this.tutorial.url ? this.sanitizer.bypassSecurityTrustResourceUrl(this.tutorial.url) : '';
   }
 
 }
