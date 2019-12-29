b54fcf5922a927d6a991fd52c08479e0ede510df
Andrzej Grabowski <andys.grabek@icloud.com>
2019-12-29 17:57:49 +0100

    Center progress indicator

--- src/app/dashboard-config/dashboard-config.component.ts
+++ src/app/dashboard-config/dashboard-config.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {UserService} from '../service/user.service';
 import * as firebase from 'firebase';
 import {TranslationBundle, TranslationService} from '../service/translation.service';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-dashboard-config',
@@ -14,11 +15,21 @@ export class DashboardConfigComponent implements OnInit {
   model: firebase.User;
   rb: TranslationBundle;
 
-  constructor(public userService: UserService, private tr: TranslationService) {
+  constructor(private userService: UserService,
+              private tr: TranslationService,
+              private matSnackBar: MatSnackBar) {
     this.rb = this.tr.getComponentBundle('DashboardConfigComponent');
   }
 
   ngOnInit() {
   }
 
+  async onChangePassword() {
+    try {
+      await this.userService.beginChangePassword();
+      this.matSnackBar.open(this.rb.get('reset-success'), undefined, {duration: 3000});
+    } catch (e) {
+      this.matSnackBar.open(this.rb.get('reset-failure'), undefined, {duration: 3000});
+    }
+  }
 }
