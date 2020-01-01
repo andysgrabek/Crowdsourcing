3390da0a55ca9a170c85731f6cdd6da5c38ca03d
Andrzej Grabowski <andys.grabek@icloud.com>
2020-01-01 18:24:06 +0100

    Add outline to hero text

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {TranslationBundle, TranslationService} from '../service/translation.service';
+import {AngularFireStorage} from '@angular/fire/storage';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -8,12 +10,17 @@ import {TranslationBundle, TranslationService} from '../service/translation.serv
 })
 export class HomeComponent implements OnInit {
   rb: TranslationBundle;
+  featureOneUrl: Observable<any>;
+  featureTwoUrl: Observable<any>;
 
-  constructor(private tr: TranslationService) {
+  constructor(private tr: TranslationService,
+              private storage: AngularFireStorage) {
     this.rb = tr.getComponentBundle('HomeComponent');
   }
 
   ngOnInit() {
+    this.featureOneUrl = this.storage.ref('assets/images/home/feature1.png').getDownloadURL();
+    this.featureTwoUrl = this.storage.ref('assets/images/home/feature2.png').getDownloadURL();
   }
 
 }
